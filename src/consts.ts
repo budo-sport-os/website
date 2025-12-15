1a503cbf40881e40066fb46ab043ad649a9de3f7
Your Name <you@example.com>
2025-12-15 21:45:05 +0100

    change title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Budo Dojo Osnabrück';
+export const SITE_DESCRIPTION = 'Willkommen im Budo Dojo Osnabrück';
