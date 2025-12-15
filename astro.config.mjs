6c129731004e84a356d63b790522a70a37ce7aff
Your Name <you@example.com>
2025-12-15 21:27:51 +0100

    rm base prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.budo-dojo-os.de',
-	base: '/',
 	integrations: [mdx(), sitemap()],
 });
